eea505ea3df888494bb3df22e788b592a0b465e8
maxazillion <47362990+maxazillion@users.noreply.github.com>
2021-02-22 18:47:49 -0500

    css styling

--- src/components/Board/Square.js
+++ src/components/Board/Square.js
@@ -1,3 +1,19 @@
-const Square = () => <div></div>;
+import PropTypes from "prop-types";
+
+const Square = ({ num, content, handler }) => (
+  <div
+    data-num={num}
+    onClick={handler}
+    className="border-2 h-24 w-24 border-black"
+  >
+    {content}
+  </div>
+);
+
+Square.propTypes = {
+  num: PropTypes.number.isRequired,
+  content: PropTypes.oneOf(["X", "O"]),
+  handler: PropTypes.func.isRequired,
+};
 
 export default Square;
--- src/components/Board/index.js
+++ src/components/Board/index.js
@@ -1,8 +1,23 @@
+import { useState } from "react";
 import Square from "./Square";
-const Board = () => (
-  <main>
-    <Square />
-  </main>
-);
+
+console.log(useState);
+
+const Board = () => {
+  const [contents, setContents] = useState(Array.from({ length: 9 }, () => ""));
+  const [current, setCurrent] = useState("X");
+
+  function handleClick(ev) {
+    console.log(ev.target);
+  }
+
+  function renderSquares(sq) {
+    return sq.map((cont, index) => (
+      <Square key={index} num={index} content={cont} handler={handleClick} />
+    ));
+  }
+
+  return <main className="container mx-auto">{renderSquares(contents)}</main>;
+};
 
 export default Board;
